bca7303757e0ac24bb67edd030d632040541981e
kau29997shal <kau29997shal.me@gmail.com>
2021-10-18 15:12:49 +0530

    start

---
+++ register.php
@@ -0,0 +1,23 @@
+<?php 
+include"head.php";
+ ?>
+ <br>
+<section id="Register" class="Register">
+      <div class="container">
+<h1>Register</h1>
+<div class="row">
+<div class="col-1"></div>
+<div class="col-4">
+<h3>Registration Form</h3>
+
+</div>
+<div class="col-6">Providing maximum details will help us maintain the quality of the profiles</div>
+<div class="col-1"></div>
+</div>
+</div>
+</section>
+
+
+ <?php 
+include"foot.php";
+ ?>
